af40ecad48e4a7a5bb779f8b1ab00d453e28d4cc
viktor <vbutovchenko@mail.ru>
2021-02-24 16:17:56 +0700

    Hw2 (#1)
    * 2 of 3
    
    * 3 of 3

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -7,21 +7,27 @@
 2. Превратите строку "01/01/20 12:10:03.234567" в объект datetime
 
 """
+from datetime import datetime, timedelta
+
 
 def print_days():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
-
+    now = datetime.now()
+    yerstaday = now - timedelta(days=1)
+    thirty_days_ago = now - timedelta(days=30)
+    print (yerstaday.strftime('%Y-%m-%d'))
+    print (now.strftime('%Y-%m-%d'))
+    print (thirty_days_ago.strftime('%Y-%m-%d'))
 
 def str_2_datetime(date_string):
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    return datetime.strptime(date_string, "%d/%m/%y %H:%M:%S.%f")
 
 if __name__ == "__main__":
     print_days()
--- 3_dict_to_csv.py
+++ 3_dict_to_csv.py
@@ -9,13 +9,26 @@
 2. Запишите содержимое списка словарей в файл в формате csv
 
 """
+import csv
+
 
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    people_list = [
+      {'name' : 'Viktor', 'age': 28, 'job' : 'specialist'}, 		
+      {'name' : 'Ivan', 'age': 18, 'job' : 'unemployed'},
+      {'name' : 'Olga', 'age': 23, 'job' : 'python developer'},
+      {'name' : 'Nikolay', 'age': 40, 'job' : 'director'},
+        ]
+    with open('export.csv', 'w', encoding='utf-8') as f:
+      fields = ['name', 'age', 'job']
+      writer = csv.DictWriter(f, fields, delimiter=';')
+      writer.writeheader()
+      for person in people_list:
+          writer.writerow(person)
 
 if __name__ == "__main__":
     main()
